00229952abee64e7a4e41e76a859bf4c97da62cd
ceyf31 <151951695+ceyf31@users.noreply.github.com>
2024-03-20 21:12:51 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://ceyfn.cloudns.org' },
+    { title: '更多内容请点击', url: 'https://ceyfn.cloudns.org' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,12 +15,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '竹杖芒鞋轻胜马',
+  HEO_HERO_TITLE_2: '一蓑烟雨任平生',
+  HEO_HERO_TITLE_3: 'ceyfn.cloudns.org',
+  HEO_HERO_TITLE_4: '可上九天揽月',
+  HEO_HERO_TITLE_5: '可下五洋捉鳖，谈笑凯歌还',
+  HEO_HERO_TITLE_LINK: 'https://ceyfn.cloudns.org',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
